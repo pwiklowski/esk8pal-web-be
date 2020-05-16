a4eff342e88dc9c1fbc900a021b043f1ca70822c
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2020-05-16 19:30:59 +0200

    Check authorize token

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import express = require("express");
+import dotenv from "dotenv";
+import * as mongo from "mongodb";
+
 const fileUpload = require("express-fileupload");
+
+const passport = require("passport");
+const BearerStrategy = require("passport-http-bearer");
+
+dotenv.config();
+
+const { OAuth2Client } = require("google-auth-library");
+const oAuth2Client = new OAuth2Client(process.env.CLIENT_ID);
 
 const { buildGPX, GarminBuilder } = require("gpx-builder");
 const parse = require("csv-parse/lib/sync");
@@ -25,14 +36,43 @@ app.use(
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+app.use(passport.initialize());
 
 (async () => {
+  const client: mongo.MongoClient = await mongo.connect("mongodb://127.0.0.1:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db("esk8pal");
+  const rides = db.collection("rides");
+  const users = db.collection("users");
+
+  passport.use(
+    new BearerStrategy(async (token: string, done: Function) => {
+      try {
+        const ticket = await oAuth2Client.verifyIdToken({
+          idToken: token,
+          audience: process.env.CLIENT_ID,
+        });
+        const payload = ticket.getPayload();
+
+        users.updateOne({ sub: payload.sub }, { $set: payload }, { upsert: true });
+
+        return done(null, payload, { scope: "all" });
+      } catch (err) {
+        return done(null, null, { scope: "all" });
+      }
+    })
+  );
+
+  app.get("/rides", passport.authenticate("bearer", { session: false }), async (req: express.Request, res: express.Response) => {
+    const allRides = await rides.find({}).toArray();
+    res.json(allRides);
+  });
+
   app.post("/convert", async (req: express.Request, res: express.Response) => {
     const records = parse((req as any).files.file.data, {
       columns: true,
